288d7f203abce5e138bd460364ca116ff0302c90
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-06-12 08:29:11 +0700

    start - project

---
+++ src/main/java/com/ducterry/base/ducterrybase/DucterryApplication.java
@@ -0,0 +1,13 @@
+package com.ducterry.base.ducterrybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucterryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DucterryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducterry/base/ducterrybase/DucterryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducterry.base.ducterrybase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DucterryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
